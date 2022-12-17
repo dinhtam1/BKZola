c8defcdd41c46e9afd576b63071a587b98c14fbf
Tam <trandinhtam911@gmail.com>
2022-12-17 22:42:14 +0700

    add MainActivity : bottom navigation

--- app/src/main/java/com/example/bkzola/firebase/MessagingService.java
+++ app/src/main/java/com/example/bkzola/firebase/MessagingService.java
@@ -3,22 +3,59 @@ package com.example.bkzola.firebase;
 
 
 
-import androidx.annotation.NonNull;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.bkzola.MainActivity;
+import com.example.bkzola.MyApplication;
+import com.example.bkzola.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Date;
+
 public class MessagingService extends FirebaseMessagingService {
 
-    @Override
-    public void onNewToken(@NonNull String token) {
-        super.onNewToken(token);
-
-    }
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage message) {
         super.onMessageReceived(message);
+        RemoteMessage.Notification notification = message.getNotification();
+        if (notification == null) {
+            return;
+        }
+        String strTitle = notification.getTitle();
+        String streMessage = notification.getBody();
 
+        sendNotification(strTitle, streMessage);
     }
+
+    private void sendNotification(String strTitle, String streMessage) {
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, MyApplication.CHANNEL_ID)
+                .setContentTitle(strTitle)
+                .setContentText(streMessage)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(resultPendingIntent);
+
+        Notification notification = notificationBuilder.build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if(notificationManager != null) {
+            notificationManager.notify(1,notification);
+        }
+    }
+
 }
+
+
+
